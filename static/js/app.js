0f2e99707ce2d71ece7603281919124f0053bd89
barzo <mustafa.kahraman@yellow.com.tr>
2013-11-28 13:28:07 +0200

    index.html & app.js new functions

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,23 @@ var Gapper = (function(module){
 
 	module.doIt = function(obj) {
 		obj.prepend( Date() + '<br />');
-	}
+	};
+
+	module.checkConnection = function() {
+		var networkState = navigator.connection.type;
+
+		var states = {};
+		states[Connection.UNKNOWN]  = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI]     = 'WiFi connection';
+		states[Connection.CELL_2G]  = 'Cell 2G connection';
+		states[Connection.CELL_3G]  = 'Cell 3G connection';
+		states[Connection.CELL_4G]  = 'Cell 4G connection';
+		states[Connection.NONE]     = 'No network connection';
+
+		alert('Connection type: ' + states[networkState]);
+	};
+
 
 	return module;
 }(Gapper || {}));
